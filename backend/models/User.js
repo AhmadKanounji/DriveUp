05008de0350aafbb657b8f2809233a15a3f7d9a0
Ahmad Kanounji <ahmadkanounji@gmail.com>
2024-04-20 20:21:20 +0300

    Forgot Password + Reset Password functionalities
    Implement the forgot password and reset password functionalities in the backend, using nodemailer which sends an email to the corresponding user with the token that will be used in the PATCH operation to reset the password

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  email: { type: String, required: true, unique: true }, // Adding email field
+  password: { type: String, required: true },
+  passwordResetToken: String,
+  passwordResetExpires: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -15,6 +19,17 @@ userSchema.pre('save', async function(next) {
 
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(20).toString('hex');
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // Token expires in 10 minutes
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,10 +2,24 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+
+
+// Setup Nodemailer transport
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAILTRAP_USERNAME,
+    pass: process.env.MAILTRAP_PASSWORD
+  }
+});
 
 router.post('/signup', async (req, res) => {
   try {
-    const user = new User(req.body);
+    const { username, email, password } = req.body;
+    const user = new User({ username, email, password });
     await user.save();
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
     res.status(201).send({ user, token });
@@ -32,5 +46,59 @@ router.post('/signin', async (req, res) => {
     res.status(400).send(error);
   }
 });
+
+router.post('/forgot-password', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send('No user found with that email');
+    }
+
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+
+    const resetURL = `http://${req.headers.host}/reset-password/${resetToken}`;
+    const message = `Forgot your password? Submit a PATCH request with your new password to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
+    await transport.sendMail({
+      to: user.email,
+      from: 'no-reply@example.com',
+      subject: 'Your password reset token (valid for 10 mins)',
+      text: message
+    });
+
+    res.status(200).send({ message: 'Token sent to email!' });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    res.status(500).send('Error sending email. Try again later.');
+  }
+});
+
+
+router.patch('/reset-password/:token', async (req, res) => {
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+
+  if (!user) {
+    return res.status(400).send('Token is invalid or has expired');
+  }
+
+  user.password = req.body.password;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  res.send('Password has been reset');
+});
+
 
 module.exports = router;
