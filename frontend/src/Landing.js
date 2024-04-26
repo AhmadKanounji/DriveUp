a40834df23c5491f6fdb9fdfb75c29e18346d798
moeram27 <mohamadr2003@gmail.com>
2024-04-27 02:59:44 +0300

    UI fixes with diff screen sizes
    Fixed the background image issues and buttons overlapping with texts on screen on smaller screens.

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -16,9 +16,10 @@ function Landing() {
       <h1>Welcome to DriveUp!</h1>
       <h2>Where you can store and access all your files securely and easily.</h2>
       <p className='p1'>DriveUp is one of the best rated online drive and storage software with up to 25Gbs of free storage!</p>
-      <div className="container">
+      <div className="button-container">
       <h4>Already a user?</h4>
       <Link to="/signIn" className="signInButton2">Sign In</Link>
+      <h4>Don't have an account?</h4>
       <Link to="/signUp" className='signUpButton2'>Sign Up For Free</Link>
       <p>Join us today for the ultimate online storage experience.</p>
 </div>
